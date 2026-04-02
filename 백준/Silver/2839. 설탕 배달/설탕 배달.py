122961e48ac8d117c213b92b1a73a8485b1117f6
garethkim12 <kimgeunha@naver.com>
2026-04-02 14:17:52 +0900

    [Silver IV] Title: 설탕 배달, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Silver/2839. 설탕 배달/설탕 배달.py
@@ -0,0 +1,16 @@
+N = int(input())
+num_bag = 0
+
+while True:
+    if N % 5 == 0:
+        num_bag += N // 5
+        break
+
+    if N < 3:
+        num_bag = -1
+        break
+
+    N -= 3
+    num_bag += 1
+
+print(num_bag)
