aab09511fa39804f43115e2b49f6dc6b316a9d5b
garethkim12 <kimgeunha@naver.com>
2026-04-02 14:01:33 +0900

    [Bronze II] Title: 블랙잭, Time: 64 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/2798. 블랙잭/블랙잭.py
+++ 백준/Bronze/2798. 블랙잭/블랙잭.py
@@ -7,16 +7,14 @@ card_list = list(map(int,input().split()))
 
 
 # 함수 정의
-def findMax():
-    max = 0
-    for card1_index in range(N-2):
-        for card2_index in range(card1_index+1, N-1):
+
+max = 0
+for card1_index in range(N-2):
+    for card2_index in range(card1_index+1, N-1):
             for card3_index in range(card2_index+1, N):
                 sum = card_list[card1_index] + card_list[card2_index] + card_list[card3_index]
-                if sum == M: return sum
-                elif sum < M and sum > max:
+                if max < sum <= M: 
                     max = sum
-    return max
 
 
-print(findMax())
+print(max)
